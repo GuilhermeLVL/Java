602df267db9d2b60fb56e3a01c7faf06b8ef30a1
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-12 14:06:59 -0300

    .

--- BarbShopCurso/src/main/java/Model/Servico.java
+++ BarbShopCurso/src/main/java/Model/Servico.java
@@ -11,7 +11,32 @@ package Model;
 public class Servico {
     private int id;
     private String descricao;   
-    private Float valor;
+    private float valor;
+
+    public Servico(int id, String descricao, float valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
     
     
 }
---
+++ BarbShopCurso/src/main/java/View/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import Model.Servico;
+
+/**
+ *
+ * @author l-0-l
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+        String nome = "Guilherme";
+        System.out.println(nome);
+        
+        Servico servico = new Servico(1,"Barba",30);
+        
+        System.out.println(servico.getDescricao());
+        System.out.print(servico.getValor());
+    }
+    
+    
+}
